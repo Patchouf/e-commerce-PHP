dd499e45bd3c88cd09e3ddcbe753019604a0554b
Yann-Fournier <y.fourni@icloud.com>
2024-01-30 16:30:10 +0100

    test mail

--- Utils.inc.php
+++ Utils.inc.php
@@ -1,6 +1,16 @@
 <?php
 
-use function PHPSTORM_META\type;
+// PHP
+// projet.e.commerce.php@gmail.com
+// ProjetECommercePHP
+
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
 $db_server = "localhost";
 $db_user = "root";
@@ -8,16 +18,16 @@ $db_pass = "";
 $db_name = "projet-ecommerce";
 $conn = "";
 
-try {
-  $conn = mysqli_connect(
-    $db_server,
-    $db_user,
-    $db_pass,
-    $db_name
-  );
-} catch (mysqli_sql_exception) {
-  echo "Could not connect! <br>";
-}
+// try {
+//   $conn = mysqli_connect(
+//     $db_server,
+//     $db_user,
+//     $db_pass,
+//     $db_name
+//   );
+// } catch (mysqli_sql_exception) {
+//   echo "Could not connect! <br>";
+// }
 
 function getNumber($table, $conn)
 {
@@ -56,15 +66,66 @@ function getToken($email, $hash, $conn)
   }
 }
 
-function EnvoieMail() {
-  ini_set("SMTP", "smtp.gmail.com");
-  ini_set('smtp_port', 1025);
-  $verif = mail("yann.fournier@ynov.com","Mail php","Voici un mail de php");
+// phpinfo();
+
+function EnvoieMail()
+{
+  $to      = 'ya.fourni@icloud.com';
+  $subject = 'Mail php';
+  $message = 'Voici un mail de php';
+
+  $verif = mail($to, $subject, $message);
+  // $verif = mail("yann.fournier@ynov.com","Mail php","Voici un mail de php");
+  // $verif = mail("ya.fourni@icloud.com","Mail php","Voici un mail de php");
   if ($verif) {
     echo "Le mail à bien été envoyer!";
   } else {
     echo "Le mail n'a pas été envoyer";
   }
 }
+
+
+// function EnvoieMail()
+// {
+//   //Create an instance; passing `true` enables exceptions
+//   $mail = new PHPMailer(true);
+
+//   try {
+//     //Server settings
+//     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+//     $mail->isSMTP();                                            //Send using SMTP
+//     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+//     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+//     $mail->Username   = 'PHP';                     //SMTP username
+//     $mail->Password   = 'ProjetECommercePHP';                               //SMTP password
+//     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+//     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+//     // Expéditeur
+//     $mail->setFrom('projet.e.commerce.php@gmail.com');
+
+//     //Destinataires
+//     $mail->addAddress('ya.fourni@icloud.com');     //Add a recipient
+//     // $mail->addAddress('ellen@example.com');               //Name is optional
+//     // $mail->addReplyTo('info@example.com', 'Information');
+//     // $mail->addCC('cc@example.com');
+//     // $mail->addBCC('bcc@example.com');
+
+//     //Attachments
+//     // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//     // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+//     //Content
+//     // $mail->isHTML(true);                                  //Set email format to HTML
+//     $mail->Subject = 'Mail from code PHP';
+//     $mail->Body    = 'This is the HTML message body <b>in bold!';
+//     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+//     $mail->send();
+//     echo 'Message has been sent';
+//   } catch (Exception $e) {
+//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+//   }
+// }
 
 EnvoieMail();
